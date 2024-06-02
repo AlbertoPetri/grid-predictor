357dd9d2086eb24160f362701295a416eb28b078
Alberto Petri <58830444+AlbertoPetri@users.noreply.github.com>
2024-06-02 18:13:24 +0200

    Updated with test images gallery

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from fastcore.all import *
 from fastai.vision.all import *
 import streamlit as st
+from streamlit_image_select import image_select
 
 
 ## Import FastAI models (inculding related methods)
@@ -39,10 +40,30 @@ def predict_grid(data):
 ## App
 st.title("Power Grid Classifier! 📈")
 bytes_data = None
+
 uploaded_image = st.file_uploader("Input a Power Grid spectrum:")
 if uploaded_image:
     bytes_data = uploaded_image.getvalue()
     st.image(bytes_data, caption="Uploaded image")   
+
+test_image = image_select(
+    label="or select a test spectrum from the gallery",
+    images=[
+        Image.open("data/SCR=4_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=7_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=12_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=16_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=30_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=40_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=60_XRR=3_SpP=30%_SpQ=0%.png"),
+        Image.open("data/SCR=80_XRR=3_SpP=30%_SpQ=0%.png"),
+    ],
+    captions=["SCR=4", "SCR=7", "SCR=12", "SCR=16", "SCR=30", "SCR=40", "SCR=60", "SCR=80"],
+    use_container_width=False,
+)
+if test_image:
+    bytes_data = test_image
+
 if bytes_data:
     analysis = st.button("Analyse!")
     if analysis:
